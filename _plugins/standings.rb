b256f76e52754df78fc18e7f25d3bc9ac5117299
Nemo <me@captnemo.in>
2024-05-08 18:57:20 +0530

    WIP switch to jekyll generated tables

---
+++ _plugins/standings.rb
@@ -0,0 +1,22 @@
+require 'jekyll'
+require 'jekyll/utils'
+
+Jekyll::Hooks.register :site, :post_read do |site|
+    site.data['results'].each do |row|
+        row['duration'] = ""
+        row['slug'] = Jekyll::Utils.slugify row['Team Name']
+        row['color_index'] = row['slug'].hash % site.data['colors'].size
+
+        if row['End Time']
+            row['d'] = d = Time.parse(row['End Time']).to_i - Time.parse(row['Start Time']).to_i
+            if d>3600
+                row['duration'] = "#{d/3600.floor}h"
+            end
+            minutes = (d/60) % 60
+            if minutes > 5
+                row['duration'] += "#{minutes}m"
+            end
+        end
+    end
+    site.data['results'].sort_by! { |s| [s['d'].nil? ? 0 : 1, s['d']] }
+end
