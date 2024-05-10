1fedcbf06c662ccda5af0f8c872c3fb5e5962e3b
Nemo <me@captnemo.in>
2024-05-10 16:20:44 +0530

    sha1 hashing

--- _plugins/standings.rb
+++ _plugins/standings.rb
@@ -1,11 +1,13 @@
 require 'jekyll'
 require 'jekyll/utils'
+require 'digest/sha1'
 
 Jekyll::Hooks.register :site, :post_read do |site|
     site.data['results'].each do |row|
         row['duration'] = ""
         row['slug'] = Jekyll::Utils.slugify row['Team Name']
-        row['color_index'] = row['slug'].hash % site.data['colors'].size
+        
+        row['color_index'] = Digest::SHA1.hexdigest(row['slug']).to_i % site.data['colors'].size
 
         if row['End Time']
             row['d'] = d = Time.parse(row['End Time']).to_i - Time.parse(row['Start Time']).to_i
