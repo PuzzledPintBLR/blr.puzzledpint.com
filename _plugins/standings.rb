966fec71d62e7cc8abeddb69015477b648d6ed51
Nemo <commits@captnemo.in>
2024-05-13 17:15:52 +0530

    Add all standings page and team links

--- _plugins/standings.rb
+++ _plugins/standings.rb
@@ -3,13 +3,22 @@ require 'jekyll/utils'
 require 'digest/sha1'
 
 Jekyll::Hooks.register :site, :post_read do |site|
+
+    def find_alias_or_slug(site, team_name)
+        slug = Jekyll::Utils.slugify team_name
+        aliased = site.data['aliases'].find { |row| row['name'] == slug }
+        if aliased
+            return aliased['slug']
+        else
+            return slug
+        end
+    end
     team_times = Hash.new { |hash, key| hash[key] = [] }
     site.data['teams'] = {}
 
     site.data['results'].each do |row|
         row['duration'] = ""
-        row['slug'] = Jekyll::Utils.slugify row['Team Name']
-        
+        row['slug'] = find_alias_or_slug(site, row['Team Name'])
         row['color_index'] = Digest::SHA1.hexdigest(row['slug']).to_i % site.data['colors'].size
 
         site.data['teams'][row['slug']] ||= {
@@ -20,8 +29,8 @@ Jekyll::Hooks.register :site, :post_read do |site|
         }
 
         if row['End Time']
-            row['d'] = d = Time.parse(row['End Time']).to_i - Time.parse(row['Start Time']).to_i
-            if d>3600
+            row['d'] = d = (Time.parse(row['End Time']).to_i - Time.parse(row['Start Time']).to_i)
+            if d>=3600
                 row['duration'] = "#{d/3600.floor}h"
             end
             minutes = (d/60) % 60
@@ -29,18 +38,22 @@ Jekyll::Hooks.register :site, :post_read do |site|
                 row['duration'] += "#{minutes}m"
             end
             site.data['teams'][row['slug']]['results'] << {
+                'd' => d/60,
                 'duration' => row['duration'],
                 'event' => row['Date'],
             }
         else
             site.data['teams'][row['slug']]['results'] << {
+                'd' => nil,
                 'duration' => '',
                 'players' => row['Team Size'],
                 'event' => row['Date'],
             }
         end
     end
-    site.data['results'].sort_by! { |s| [s['d'].nil? ? 0 : 1, s['d']] }
+    site.data['results'].sort_by! do |r|
+        [r['d'].nil? ? 1 : 0, r['d'], r['Start Time']]
+    end
     site.data['teams'] = site.data['teams'].values
     site.data['teams'].sort_by! {|r| -r['results'].size }
 end
