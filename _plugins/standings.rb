60c1fa0b15e3e7f872ba93b6aef9eb4c11c4666d
Nemo <me@captnemo.in>
2024-05-13 16:02:18 +0530

    WIP team listing page

--- _plugins/standings.rb
+++ _plugins/standings.rb
@@ -3,11 +3,21 @@ require 'jekyll/utils'
 require 'digest/sha1'
 
 Jekyll::Hooks.register :site, :post_read do |site|
+    team_times = Hash.new { |hash, key| hash[key] = [] }
+    site.data['teams'] = {}
+
     site.data['results'].each do |row|
         row['duration'] = ""
         row['slug'] = Jekyll::Utils.slugify row['Team Name']
         
         row['color_index'] = Digest::SHA1.hexdigest(row['slug']).to_i % site.data['colors'].size
+
+        site.data['teams'][row['slug']] ||= {
+            'results' => [],
+            'slug' => row['slug'],
+            'color_index'=> row['color_index'],
+            'name' => row['Team Name']
+        }
 
         if row['End Time']
             row['d'] = d = Time.parse(row['End Time']).to_i - Time.parse(row['Start Time']).to_i
@@ -18,7 +28,19 @@ Jekyll::Hooks.register :site, :post_read do |site|
             if minutes > 5
                 row['duration'] += "#{minutes}m"
             end
+            site.data['teams'][row['slug']]['results'] << {
+                'duration' => row['duration'],
+                'event' => row['Date'],
+            }
+        else
+            site.data['teams'][row['slug']]['results'] << {
+                'duration' => '',
+                'players' => row['Team Size'],
+                'event' => row['Date'],
+            }
         end
     end
     site.data['results'].sort_by! { |s| [s['d'].nil? ? 0 : 1, s['d']] }
+    site.data['teams'] = site.data['teams'].values
+    site.data['teams'].sort_by! {|r| -r['results'].size }
 end
