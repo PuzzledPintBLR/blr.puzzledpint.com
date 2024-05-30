7108bee52784bd1817597d54e72e72549fd822b6
Nemo <commits@captnemo.in>
2024-05-30 14:24:00 +0530

    Adds some initial badge list

--- _plugins/duration.rb
+++ _plugins/duration.rb
@@ -1,8 +1,10 @@
 module Jekyll
   module Duration
     def duration_to_string(seconds)
-      if seconds < 3600
+      if seconds < 3600 # <1h
         Time.at(seconds, in: '+00:00').strftime("%-Mm")
+      elsif seconds %3600 == 0 # exact hour
+        Time.at(seconds, in: '+00:00').strftime("%-Hh")
       else
         Time.at(seconds, in: '+00:00').strftime("%-Hh%-Mm")
       end
